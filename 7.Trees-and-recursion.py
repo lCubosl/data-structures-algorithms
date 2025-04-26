2f99f3a788256b39dead966f0f707c8ed8473426
shifter <muislarques@gmail.com>
2025-04-26 21:32:11 +0100

    tree-map, recursion

---
+++ 7.Trees-and-recursion.py
@@ -0,0 +1,7 @@
+# Trees and recursion
+# A tree is a data structure that consists of node at different levels.
+# A tree can represent a hierarchy thorugh connections between
+# nodes of different levels
+
+image = "tree-map.png"
+
