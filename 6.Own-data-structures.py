971a6900efdb8471696f8acf14928f30109ee420
shifter <muislarques@gmail.com>
2025-04-26 21:18:02 +0100

    6. mode

--- 6.Own-data-structures.py
+++ 6.Own-data-structures.py
@@ -179,3 +179,33 @@ s.push(4)
 s.push_many(2, 5)
 print(s)
 print("--------------------------------------------------")
+
+# Example: Mode
+# Implement a class Mode with the following methods
+  # add(x): add number x on the list
+  # mode(): return the mode of the list, the most frequent number on the list
+# time complexity O(1)
+
+class Mode:
+  def __init__(self):
+    self.count = {}
+    self.status = (0, 0)
+
+  def add(self, x):
+    if x not in self.count:
+      self.count[x] = 0
+    self.count[x] += 1
+    self.status = max(self.status, (self.count[x], x))
+  
+  def mode(self):
+    return self.status[1]
+  
+m = Mode()
+m.add(1)
+m.add(1)
+m.add(2)
+print(m.mode()) # 1
+m.add(2)
+m.add(2)
+print(m.mode()) # 2
+print("--------------------------------------------------")
