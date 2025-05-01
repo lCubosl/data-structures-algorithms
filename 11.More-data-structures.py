8b0621e8c5762d59bdc5feb89e3f2e3828b946c9
LuisM <muislarques@gmail.com>
2025-05-01 03:02:36 +0100

    more data structures begining. deque and head

---
+++ 11.More-data-structures.py
@@ -0,0 +1,6 @@
+# 11. More data structures
+
+# this chapter is about 2 otehr data structures:
+  # deque: list structure that supports efficient addition and removal of elements both at the 
+  # beggining and end of a list
+  # heap: data structure where the smallest or the largest element can be found and removed efficiently
