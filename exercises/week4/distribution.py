595970ec98889b61915f7eb1ed3800fd7eb61b83
LuisM <muislarques@gmail.com>
2025-05-10 16:48:25 +0100

    distribution

--- exercises/week4/distribution.py
+++ exercises/week4/distribution.py
@@ -1,16 +1,37 @@
 # Your task is to computer the length distribution of the distinct substrings of a given string.
-#For example, the distinct substrings of the string abab are a, b, ab, ba, aba, bab and abab. Their length distribution is as follows:
+
+# For example, the distinct substrings of the string abab are a, b, ab, ba, aba, bab and abab. Their length 
+# distribution is as follows:
 
 # length 1: substring count 2
 # length 2: substring count 2
 # length 3: substring count 2
 # length 4: substring count 1
 
-# In a file distribution.py, implement the function create_distribution that returns the distinct substring length distribution for the string given as a parameter. The distribution must be returned as a dictionary as shown in the examples.
-# The function will be tested in various situations, where the string is of length at most 20 and consists of the characters a–z. The function should be efficient in all such situations.
+# In a file distribution.py, implement the function create_distribution that returns the distinct substring length 
+# distribution for the string given as a parameter. The distribution must be returned as a dictionary as shown in the 
+# examples.
+
+# The function will be tested in various situations, where the string is of length at most 20 and consists of the 
+# characters a–z. The function should be efficient in all such situations.
 
 def create_distribution(string):
-  pass
+  substrings = set()
+  distribution = {}
+
+  for i in range(len(string)):
+    for j in range(i, len(string)):
+      substrings.add(string[i:j+1])
+  
+
+  for substring in substrings:
+    length = len(substring)
+    if length in distribution:
+      distribution[length] +=1
+    else:
+      distribution[length] =1
+
+  return distribution
 
 print(create_distribution("aaaa"))  # {1: 1, 2: 1, 3: 1, 4: 1}
 print(create_distribution("abab"))  # {1: 2, 2: 2, 3: 2, 4: 1}
