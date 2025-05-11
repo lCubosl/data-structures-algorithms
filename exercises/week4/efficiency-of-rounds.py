a4cb681cc1cde528f592d6a21d86357eb560d977
LuisM <muislarques@gmail.com>
2025-05-11 01:53:41 +0100

    efficiency-of-rounds transcript

---
+++ exercises/week4/efficiency-of-rounds.py
@@ -0,0 +1,18 @@
+# In the course material, there is an example for week 4 of an algorithm using a dictionary for counting the rounds 
+# needed to collect numbers from a list. Earlier in an exercise for week 2, the same task was solved using a list 
+# instead of a dictionary.
+
+# The auxiliary data structure used in the task can be either a list or a dictionary, because a number can be used 
+# either as a list index or as a dictionary key. How does the choice of the data structure affect the efficiency of 
+# the algorithm?
+
+# Compare the efficiency of the two implementations for an input list containing the numbers 1,2,\dots,10^7 in a 
+# random order.
+
+# In this task you get a point automatically, when you fill in your results and the code you used, and press the 
+# submit button.
+
+# Running time when using a list:  ANSWER
+# Running time when using a dictionary:  ANSWER
+
+# The code used in the test:
