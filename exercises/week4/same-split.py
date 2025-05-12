52f9c337ec28667a4e8c1017c10cc09c5337e592
LuisM <muislarques@gmail.com>
2025-05-12 12:34:59 +0100

    same split

--- exercises/week4/same-split.py
+++ exercises/week4/same-split.py
@@ -10,8 +10,44 @@
 # The function should be efficient even for long lists. The last two test cases in the code template are long lists 
 # and the function should finish quickly in these cases too.
 
+# O(n^2) because inside of the for loop, numbers arr is being split (additional O(n))
+# O(n) x O(n) = O(n x n) = O(n^2)
 def count_splits(numbers):
-  pass
+  count = 0
+  n = len(numbers)
+
+  for i in range(1, n):
+    if set(numbers[:i]) == set(numbers[i:]):
+      count += 1
+  
+  return count
+
+# O(n)
+def count_splits(numbers):
+  count = 0
+  n = len(numbers)
+
+  right_count = {}
+  for num in numbers:
+    if num in right_count:
+      right_count[num] += 1
+    else:
+      right_count[num] = 1
+  
+  right_seen = set(right_count.keys())
+  left_seen = set()
+
+  for i in range(n):
+    num = numbers[i]
+    left_seen.add(num)
+
+    right_count[num] -= 1
+    if right_count[num] == 0:
+      right_seen.remove(num)
+
+    if left_seen == right_seen:
+      count += 1
+  return count
 
 print(count_splits([1, 1, 1, 1])) # 3
 print(count_splits([1, 1, 2, 1])) # 0
