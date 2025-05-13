5eb522372cf177dc2deffa2c41d1550fb244d463
LuisM <muislarques@gmail.com>
2025-05-13 14:18:09 +0100

    collision new wrong answer

--- exercises/week4/collision.py
+++ exercises/week4/collision.py
@@ -61,35 +61,38 @@ def hash_value(string):
   return sum(nums)#%(2**32)
 
 def find_other(string):
-  number = hash_value(string) # 2905682
-  new_nums = []
-  length_new_nums = 0
+  A = 23
+  M = 2**32
+  target = hash_value(string)
+  digits = []
 
-  while number > 0:
-    power = 23 ** length_new_nums
-    max_digit = min(25, number // power)
+  powers = []
+  power = 1
 
-    if  number < power:
-      new_nums.append(0)
-    elif number <= 25 * power:
-      new_nums.append(number // power)
-      break
-    else:
-      sub = random.randint(0,max_digit)
-      new_nums.append(sub)
-      number -= (sub * power)
-      length_new_nums += 1
+  while power <= target:
+    powers.append(power)
+    power *= A
+  powers.reverse()
 
-  print(new_nums, number, "".join(chr(ord("a") + x) for x in new_nums))
-  return new_nums
+  number = target
+  for p in powers:
+    max_digit = min(25, number // p)
+    digit = random.randint(0, max_digit)
+    digits.append(digit)
+    number -= digit * p
+  
+  if number != 0:
+    return find_other(string)
+  
+  return ''.join(chr(ord('a') + d) for d in digits)
 
 # print(hash_value1("kissaabcdefghijklmnopqrstuvwxyz")) # 2905682
 # print(hash_value2("kissaabcdefghijklmnopqrstuvwxyz")) # 2905682
 
-string1 = "kbgvj"
+string1 = "kissa"
 string2 = find_other("kissa")
 print(string1, hash_value(string1)) # kissa 2905682
-#print(string2, hash_value(string2)) # zfgjynuk 2905682
+print(string2, hash_value(string2)) # zfgjynuk 2905682
 
 # In the above code, the string zfgjynuk is an example of what the function might return. Your function does not need 
 # to return this specific string. It can return any string that satisfies the requirements and has the same hash value.
