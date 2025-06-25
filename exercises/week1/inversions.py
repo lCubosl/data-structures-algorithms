6919fd5940d2fadae081084f0c98b98f2ce6d011
shifter <muislarques@gmail.com>
2025-06-25 01:16:45 +0100

    inversions.py O(n2)

--- exercises/week1/inversions.py
+++ exercises/week1/inversions.py
@@ -1,11 +1,23 @@
-# You are given a list that consists of the numbers 1 \dots n. A pair of indices (i,j) is an inversion if i<j and the element at index i on the 
+# You are given a list that consists of the numbers 1 ... n. A pair of indices (i,j) is an inversion if i<j and the element at index i on the 
 # list is greater than the element at index j.
 
 # You may assume that n is at most 100.
 # In a file inversions.py, implement a function count that returns the total number of inversions in the list.
 
 def count(t):
-  pass
+  # base case, length of array is 0 or 1
+  if len(t) <= 1:
+    return 0
+  
+  # O(n**2) not optimal for big algorithms
+  inversion = 0
+  n = len(t)
+  for i in range(n):
+    for j in range(i + 1, n):
+      if t[i] > t[j]:
+        inversion += 1
+  
+  return inversion
 
 print(count([1,3,2])) # 1
 print(count([1])) # 0
